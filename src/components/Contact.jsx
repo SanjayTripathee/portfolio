d446efe77c47af8d2b82a677d05baded12a3bc87
u20Sanjay <9864226384s@gmail.com>
2025-08-22 07:21:57 +1000

    managing constant.js file

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,8 +1,43 @@
 import React from 'react';
 import './CSS/contact.css';
 import Footer from './Footer';
+import axios from 'axios';
+import { useState } from 'react';
+import { bUrl } from '../../constant';
 
 const Contact = () => {
+  // form state
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+ const handleSubmit = async (e) => {
+  e.preventDefault();
+
+  if (!name || !email || !message) {
+    alert("All fields are required");
+    return;
+  }
+
+  const data = { name, email, message };
+
+  try {
+    const result = await axios({
+      url: `${bUrl}/portfolio`, // your backend endpoint
+      method: "post",
+      data: data, // form data to save in DB
+    });
+
+    alert("Message sent successfully!");
+    setName("");
+    setEmail("");
+    setMessage("");
+    console.log(result.data); // optional: see backend response
+  } catch (error) {
+    alert(error.response?.data?.error || "Failed to send message");
+    console.error(error);
+  }
+};
   return (
     <>
         <section className="contact-container">
@@ -21,10 +56,28 @@ const Contact = () => {
 
       <div className="contact-right">
         <h1>Get in <span className="highlight">touch</span></h1>
-        <form>
-          <input type="text" placeholder="Enter your name" required />
-          <input type="email" placeholder="Enter your email" required />
-          <textarea placeholder="Enter your message" required />
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Enter your name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+          />
+      <input
+        type="email"
+        placeholder="Enter your email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        required
+      />
+      <textarea
+        placeholder="Enter your message"
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+        required
+      />
+
           <button type="submit">Submit now</button>
         </form>
       </div>
